b1b6621c9f1f6cded6b6bfb23cfc1bc0a537e9c2
Dan Kovalsky <dankov@wix.com>
2021-03-08 16:08:50 +0200

    mock implementation for registerComponentListener. and bump version 18

--- src/NavigationMock.tsx
+++ src/NavigationMock.tsx
@@ -136,6 +136,7 @@ class NativeNavigationMock {
       registerCommandListener: () => {
       },
       registerComponentListener: () => {
+        return {remove : () => {}}
       },
       registerModalDismissedListener: () => {
       },
